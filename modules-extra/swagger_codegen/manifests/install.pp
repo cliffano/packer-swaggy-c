7121189ed427274f3b84a3d59d439ff67cad4aad
Cliffano Subagio <cliffano@gmail.com>
2017-06-13 23:19:00 +1000

    Move git install to main manifest. Rename base_dir to repo_dir.

--- modules-extra/swagger_codegen/manifests/install.pp
+++ modules-extra/swagger_codegen/manifests/install.pp
@@ -1,14 +1,9 @@
 class swagger_codegen::install (
   $revision = 'master',
-  $base_dir = '/opt/swagger-codegen',
+  $repo_dir = '/opt/swagger-codegen',
 ) {
 
-  package { 'git':
-    ensure   => '2.13.0-r0',
-    provider => 'apk',
-  }
-
-  vcsrepo { "${base_dir}/swagger-codegen":
+  vcsrepo { "${repo_dir}":
     ensure   => present,
     provider => git,
     source   => 'git://github.com/swagger-api/swagger-codegen.git',
@@ -20,7 +15,7 @@ class swagger_codegen::install (
   }
 
   exec { '/usr/bin/mvn package -Dmaven.test.skip=true':
-    cwd => "${base_dir}/swagger-codegen",
+    cwd => $repo_dir,
   }
 
 }
