987108772d9ca28eec41eca8a5b8a8941689bcb7
Cliffano Subagio <cliffano@gmail.com>
2017-06-13 23:20:18 +1000

    Move swaggy-c installation to an extra module.

---
+++ modules-extra/swaggy_c/manifests/init.pp
@@ -0,0 +1,3 @@
+class swaggy_c (
+) {
+}
---
+++ modules-extra/swaggy_c/manifests/install.pp
@@ -0,0 +1,22 @@
+class swaggy_c::install (
+  $revision = 'master',
+  $repo_dir = '/opt/swaggy_c',
+) {
+
+  vcsrepo { "${repo_dir}":
+    ensure   => present,
+    provider => git,
+    source   => 'git://github.com/cliffano/swaggy-c.git',
+    revision => 'master',
+  }
+
+  exec { '/usr/bin/npm link':
+    cwd => $repo_dir,
+  }
+
+  file { '/usr/local/bin/swaggy-c':
+    ensure => link,
+    target => "${repo_dir}/bin/swaggy-c",
+  }
+
+}
