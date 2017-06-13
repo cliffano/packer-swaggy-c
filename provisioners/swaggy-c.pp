368dc90c39dbdd6b45212f44be4579fd73855245
Cliffano Subagio <cliffano@gmail.com>
2017-06-13 17:14:17 +1000

    Switch swaggy-c dir to /opt/swaggy-c . Fix npm command path.

--- provisioners/swaggy-c.pp
+++ provisioners/swaggy-c.pp
@@ -1,13 +1,13 @@
 include swagger_codegen::langs
 include swagger_codegen::install
 
-vcsrepo { '/tmp/swaggy-c':
+vcsrepo { '/opt/swaggy-c':
   ensure   => present,
   provider => git,
   source   => 'git://github.com/cliffano/swaggy-c.git',
   revision => 'master',
 }
 
-exec { '/usr/npm link':
-  cwd => '/tmp/swaggy-c',
+exec { '/usr/bin/npm link':
+  cwd => '/opt/swaggy-c',
 }
