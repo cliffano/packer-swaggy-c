4833a9b59bf4aeea6305c3708c72ced178ca9942
Cliffano Subagio <cliffano@gmail.com>
2017-06-14 00:48:40 +1000

    Add make installation.

--- provisioners/main.pp
+++ provisioners/main.pp
@@ -3,6 +3,11 @@ package { 'git':
   provider => 'apk',
 }
 
+package { 'make':
+  ensure   => '4.2.1-r0',
+  provider => 'apk',
+}
+
 include swagger_codegen::langs
 include swagger_codegen::install
 include swaggy_c::install
