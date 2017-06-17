a1ace7e6893aa1483b7a8a63254e494c973a39a3
Cliffano Subagio <cliffano@gmail.com>
2017-06-17 23:43:00 +1000

    Add libffi-dev package.

--- modules-extra/swagger_codegen/manifests/init.pp
+++ modules-extra/swagger_codegen/manifests/init.pp
@@ -1,10 +1,16 @@
 class swagger_codegen (
   $buildbase_version = '0.5-r0',
+  $libffidev_version = '3.2.1-r3',
   $base_dir          = '/opt/swagger-codegen',
 ) {
 
   package { 'build-base':
     ensure   => $buildbase_version,
+    provider => 'apk',
+  }
+
+  package { 'libffi-dev':
+    ensure   => $libffidev_version,
     provider => 'apk',
   }
 
