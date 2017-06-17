493c8894d785d8369542fd82c65a0cddbe50764d
Cliffano Subagio <cliffano@gmail.com>
2017-06-17 17:37:37 +1000

    Include all current langs into main manifest.

--- provisioners/main.pp
+++ provisioners/main.pp
@@ -10,6 +10,13 @@ package { 'make':
 
 include swagger_codegen
 include swagger_codegen::install
+include swagger_codegen::langs::erlang
+include swagger_codegen::langs::go
+include swagger_codegen::langs::java
 include swagger_codegen::langs::javascript
+include swagger_codegen::langs::perl
+include swagger_codegen::langs::php
+include swagger_codegen::langs::python
 include swagger_codegen::langs::ruby
+
 include swaggy_c::install
