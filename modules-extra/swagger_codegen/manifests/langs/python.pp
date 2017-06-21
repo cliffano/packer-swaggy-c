9aca7b7e12f8367e6ac6c01642d6b1dfedc3bf82
Cliffano Subagio <cliffano@gmail.com>
2017-06-21 13:46:39 +1000

    Add pip package to python lang.

--- modules-extra/swagger_codegen/manifests/langs/python.pp
+++ modules-extra/swagger_codegen/manifests/langs/python.pp
@@ -1,10 +1,16 @@
 class swagger_codegen::langs::python (
+  $pip_version    = '8.1.2-r0',
   $python_version = '2.7.12-r0',
   $bin_dir        = '/opt/swagger-codegen/bin/',
 ) {
 
   package { 'python':
     ensure   => $python_version,
+    provider => 'apk',
+  }
+
+  package { 'py-pip':
+    ensure   => $pip_version,
     provider => 'apk',
   }
 
