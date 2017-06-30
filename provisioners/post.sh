3a4d2ec86b2569743e1c9eb7678c91c40db7053b
Cliffano Subagio <cliffano@gmail.com>
2017-07-01 09:53:30 +1000

    Add more comments.

--- provisioners/post.sh
+++ provisioners/post.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+# Log versions info for all langs and tools installed
 /opt/swagger-codegen/bin/info.sh
 /opt/swaggy-c/bin/info.sh
 
+# Clean up stage directory
 rm -rf /tmp/packer-swaggy-c/
--- provisioners/pre.sh
+++ provisioners/pre.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Puppet on Alpine Linux installation is based on:
 # https://puppet.com/blog/using-puppet-alpine-linux
 
 echo http://dl-4.alpinelinux.org/alpine/edge/testing/ >> /etc/apk/repositories
@@ -10,9 +11,13 @@ gem install puppet
 ln -s /usr/lib/ruby/gems/2.4.0/gems/puppet-4.10.1/bin/puppet /usr/local/bin/puppet
 ln -s /usr/lib/ruby/gems/2.4.0/gems/facter-2.4.6/bin/facter /usr/local/bin/facter
 
+echo "****************************************"
+echo "* Host info"
+echo "****************************************"
 echo "PATH: $PATH"
 echo "puppet version: $(puppet --version)"
 echo "facter version: $(facter --version)"
 facter os
 
+# Create stage directory for provisioning using puppet-masterless
 mkdir -p /tmp/packer-swaggy-c/
