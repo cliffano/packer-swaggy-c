632901dfb3bac41e7f15cd0210f96b811b213a2b
Cliffano Subagio <cliffano@gmail.com>
2017-06-30 20:39:57 +1000

    Install ruby-json and ruby-rdoc as pre-req for puppet installation, prior to langs provisioning.

--- provisioners/pre.sh
+++ provisioners/pre.sh
@@ -4,7 +4,7 @@
 
 echo http://dl-4.alpinelinux.org/alpine/edge/testing/ >> /etc/apk/repositories
 apk update
-apk add sudo shadow ruby less bash
+apk add sudo shadow ruby ruby-json ruby-rdoc less bash
 gem install puppet
 
 ln -s /usr/lib/ruby/gems/2.4.0/gems/puppet-4.10.1/bin/puppet /usr/local/bin/puppet
