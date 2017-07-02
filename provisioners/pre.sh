14232821e88a34682d5836bf2c6d9e597314ebd6
Cliffano Subagio <cliffano@gmail.com>
2017-07-03 06:09:23 +1000

    Modify installation sequence based on Puppetlabs' Docker experimental images.

--- provisioners/pre.sh
+++ provisioners/pre.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
+# Create stage directory for provisioning using puppet-masterless
+mkdir -p /tmp/packer-swaggy-c/
+cd /tmp/packer-swaggy-c/
+
+# Install Puppet, adapted from
+# https://github.com/puppetlabs/puppet-in-docker/blob/master/puppet-agent-ubuntu/Dockerfile
+# Will switch to Puppetlabs' Docker image if they provide puppet-masterless
 apt-get update
-apt-get install puppet
+apt-get install --no-install-recommends -y wget  ca-certificates lsb-release sudo apt-utils
+wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
+dpkg -i puppetlabs-release-pc1-xenial.deb
+rm puppetlabs-release-pc1-xenial.deb
+apt-get update
+apt-get install --no-install-recommends -y puppet-agent
+rm -rf /var/lib/apt/lists/*
+PATH=/opt/puppetlabs/server/bin:/opt/puppetlabs/puppet/bin:/opt/puppetlabs/bin:$PATH
 
 echo "****************************************"
 echo "* Host info"
@@ -9,7 +23,5 @@ echo "****************************************"
 echo "PATH: $PATH"
 echo "puppet version: $(puppet --version)"
 echo "facter version: $(facter --version)"
+echo "hiera version: $(hiera --version)"
 facter os
-
-# Create stage directory for provisioning using puppet-masterless
-mkdir -p /tmp/packer-swaggy-c/
