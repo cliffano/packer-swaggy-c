f43c3deaee82f530ae3bf7c77411525072a4e886
Cliffano Subagio <cliffano@gmail.com>
2017-07-04 00:12:26 +1000

    Comment out langs that haven't been transitioned to ubuntu.

--- provisioners/main.pp
+++ provisioners/main.pp
@@ -1,17 +1,15 @@
-include git
-
 include swagger_codegen
 include swagger_codegen::install
 include swagger_codegen::langs::erlang
 include swagger_codegen::langs::go
 include swagger_codegen::langs::java
-include swagger_codegen::langs::javascript
-include swagger_codegen::langs::perl
-include swagger_codegen::langs::php
-include swagger_codegen::langs::python
-include swagger_codegen::langs::ruby
+#include swagger_codegen::langs::javascript
+#include swagger_codegen::langs::perl
+#include swagger_codegen::langs::php
+#include swagger_codegen::langs::python
+#include swagger_codegen::langs::ruby
 #include swagger_codegen::langs::scala
-include swagger_codegen::langs::typescript
+#include swagger_codegen::langs::typescript
 
-include swaggy_c
-include swaggy_c::install
+#include swaggy_c
+#include swaggy_c::install
