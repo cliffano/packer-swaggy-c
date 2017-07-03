e5a49823271cf305a448fedc84f8f18f03c7db6b
Cliffano Subagio <cliffano@gmail.com>
2017-07-04 00:12:58 +1000

    Remove oraclejdk, default to openjdk for now.

--- modules-extra/swagger_codegen/manifests/langs/java.pp
+++ modules-extra/swagger_codegen/manifests/langs/java.pp
@@ -1,15 +1,10 @@
 class swagger_codegen::langs::java (
-  $jdk8_version_major = '8u101',
-  $jdk8_version_minor = 'b13',
-  $maven_version      = '3.5.0',
-  $bin_dir            = '/opt/swagger-codegen/bin/',
+  $maven_version = '3.5.0',
+  $bin_dir       = '/opt/swagger-codegen/bin/',
 ) {
 
-  java::oracle { 'jdk8' :
-    ensure        => 'present',
-    version_major => $jdk8_version_major,
-    version_minor => $jdk8_version_minor = 'b13',
-    java_se       => 'jdk',
+  class { 'java':
+    distribution => 'jdk',
   }
 
   class { 'maven::maven':
