13abf96bad360429b90a90f4f8e7a3225b1a84dc
Cliffano Subagio <cliffano@gmail.com>
2017-07-05 17:06:24 +1000

    Re-enable erlang and perl after modules switch.

--- provisioners/main.pp
+++ provisioners/main.pp
@@ -12,15 +12,15 @@ include git
 
 include swagger_codegen
 include swagger_codegen::install
-#include swagger_codegen::langs::erlang
+include swagger_codegen::langs::erlang
 include swagger_codegen::langs::go
 include swagger_codegen::langs::java
 include swagger_codegen::langs::javascript
 #include swagger_codegen::langs::perl
-#include swagger_codegen::langs::php
+include swagger_codegen::langs::php
 include swagger_codegen::langs::python
 include swagger_codegen::langs::ruby
-include swagger_codegen::langs::scala
+#include swagger_codegen::langs::scala
 include swagger_codegen::langs::typescript
 
 include swaggy_c
