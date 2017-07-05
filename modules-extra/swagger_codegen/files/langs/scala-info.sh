399728d0555700756aba900c039ef220f2d87a55
Cliffano Subagio <cliffano@gmail.com>
2017-07-05 17:02:20 +1000

    Fix scala version flag. Remove gt8.

--- modules-extra/swagger_codegen/files/langs/scala-info.sh
+++ modules-extra/swagger_codegen/files/langs/scala-info.sh
@@ -5,6 +5,5 @@ set -o errexit
 echo "****************************************"
 echo "* Scala info"
 echo "****************************************"
-echo "scala version: $(scala --version)"
+echo "scala version: $(scala -version)"
 echo "sbt version: $(sbt --version)"
-echo "gt8 version: $(gt8 --version)"
