b884c4d3dfe7ab81a24349702d0423e564c9e99c
Cliffano Subagio <cliffano@gmail.com>
2017-07-08 09:08:04 +1000

    Provision sdkman and kotlin.

--- modules-extra/swagger_codegen/manifests/langs/kotlin.pp
+++ modules-extra/swagger_codegen/manifests/langs/kotlin.pp
@@ -2,6 +2,31 @@ class swagger_codegen::langs::kotlin (
   $bin_dir = '/opt/swagger-codegen/bin/',
 ) {
 
+  include swagger_codegen::langs::java
+
+  package { 'unzip':
+    ensure   => latest,
+    provider => apt,
+  }
+
+  package { 'zip':
+    ensure   => latest,
+    provider => apt,
+  }
+
+  package { 'curl':
+    ensure   => latest,
+    provider => apt,
+  }
+
+  include sdkman
+
+  sdkman::package { 'kotlin':
+    version    => '1.1.3',
+    is_default => true,
+    ensure     => present,
+  }
+
   file { "${bin_dir}/kotlin-info.sh":
     ensure => present,
     source => 'puppet:///modules/swagger_codegen/langs/kotlin-info.sh',
