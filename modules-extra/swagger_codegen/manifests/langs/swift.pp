fd50d3c1236cd1a08a99feab653ecf87619cffcf
Cliffano Subagio <cliffano@gmail.com>
2017-07-08 20:34:36 +1000

    Add swift binary download.

--- modules-extra/swagger_codegen/manifests/langs/swift.pp
+++ modules-extra/swagger_codegen/manifests/langs/swift.pp
@@ -1,6 +1,16 @@
 class swagger_codegen::langs::swift (
-  $bin_dir = '/opt/swagger-codegen/bin/',
+  $bin_dir    = '/opt/swagger-codegen/bin/',
+  $langs_dir  = '/opt/swagger-codegen/langs/',
+  $source_url = 'https://swift.org/builds/ubuntu1510/swift-2.2-SNAPSHOT-2015-12-10-a/swift-2.2-SNAPSHOT-2015-12-10-a-ubuntu15.10.tar.gz',
 ) {
+
+  archive { $langs_dir:
+    ensure        => present,
+    extract       => true,
+    extract_path  => $langs_dir,
+    source        => $source_url,
+    cleanup       => true,
+  }
 
   file { "${bin_dir}/swift-info.sh":
     ensure => present,
