d25f2d0a58c7360a2b16f8313ab3fd9991759d1e
Cliffano Subagio <cliffano@gmail.com>
2017-07-09 15:54:00 +1000

    Add sinatra provisioning steps.

--- modules-extra/swagger_codegen/manifests/langs/sinatra.pp
+++ modules-extra/swagger_codegen/manifests/langs/sinatra.pp
@@ -2,6 +2,8 @@ class swagger_codegen::langs::sinatra (
   $bin_dir = '/opt/swagger-codegen/bin/',
 ) {
 
+  include sinatra
+  
   file { "${bin_dir}/sinatra-info.sh":
     ensure => present,
     source => 'puppet:///modules/swagger_codegen/langs/sinatra-info.sh',
