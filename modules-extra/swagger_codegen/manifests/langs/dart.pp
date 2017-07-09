6f9364b4ec594f10c3c47e5412bb8e01d6df4c1f
Cliffano Subagio <cliffano@gmail.com>
2017-07-09 15:53:45 +1000

    Add dart provisioning steps.

--- modules-extra/swagger_codegen/manifests/langs/dart.pp
+++ modules-extra/swagger_codegen/manifests/langs/dart.pp
@@ -1,6 +1,23 @@
+# Adapted from https://github.com/dartsim/dart/wiki/DART-5.0-Installation-for-Ubuntu
 class swagger_codegen::langs::dart (
   $bin_dir = '/opt/swagger-codegen/bin/',
 ) {
+
+  apt::ppa { 'ppa:libccd-debs/ppa': }
+
+  apt::ppa { 'ppa:fcl-debs/ppa': }
+
+  apt::ppa { 'ppa:dartsim/ppa': }
+
+  package { 'libdart-core5-dev':
+    ensure   => latest,
+    provider => apt,
+  }
+
+  package { 'libdart5-dev':
+    ensure   => latest,
+    provider => apt,
+  }
 
   file { "${bin_dir}/dart-info.sh":
     ensure => present,
