b07f34db873ab11acd813f5f7f3e184b09d879e1
Cliffano Subagio <cliffano@gmail.com>
2017-07-10 17:09:10 +1000

    Replace swift-info file with a template.

--- modules-extra/swagger_codegen/manifests/langs/swift.pp
+++ modules-extra/swagger_codegen/manifests/langs/swift.pp
@@ -1,8 +1,9 @@
 # Adapted from https://itsfoss.com/use-swift-linux/
 class swagger_codegen::langs::swift (
-  $bin_dir    = '/opt/swagger-codegen/bin/',
-  $langs_dir  = '/opt/swagger-codegen/langs/',
-  $source_url = 'https://swift.org/builds/swift-3.1.1-release/ubuntu1604/swift-3.1.1-RELEASE/swift-3.1.1-RELEASE-ubuntu16.04.tar.gz',
+  $bin_dir     = '/opt/swagger-codegen/bin/',
+  $langs_dir   = '/opt/swagger-codegen/langs/',
+  $archive_dir = 'swift-3.1.1-RELEASE-ubuntu16.04',
+  $source_url  = 'https://swift.org/builds/swift-3.1.1-release/ubuntu1604/swift-3.1.1-RELEASE/swift-3.1.1-RELEASE-ubuntu16.04.tar.gz',
 ) {
 
   package { 'clang':
@@ -24,9 +25,11 @@ class swagger_codegen::langs::swift (
   }
 
   file { "${bin_dir}/swift-info.sh":
-    ensure => present,
-    source => 'puppet:///modules/swagger_codegen/langs/swift-info.sh',
-    mode   => '0755',
+    ensure  => present,
+    content => epp('swagger_codegen/langs/swift-info.sh.epp', {
+      bin_dir => "${langs_dir}/${archive_dir}/usr/bin",
+    }),
+    mode    => '0755',
   }
 
 }
