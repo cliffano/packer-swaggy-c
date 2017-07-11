43b5b3336485ea6cdab90161becdba2f365ed316
Cliffano Subagio <cliffano@gmail.com>
2017-07-11 21:12:20 +1000

    Fix Swift download and unpack.

--- modules-extra/swagger_codegen/manifests/langs/swift.pp
+++ modules-extra/swagger_codegen/manifests/langs/swift.pp
@@ -1,9 +1,11 @@
 # Adapted from https://itsfoss.com/use-swift-linux/
 class swagger_codegen::langs::swift (
-  $bin_dir     = '/opt/swagger-codegen/bin/',
-  $langs_dir   = '/opt/swagger-codegen/langs/',
-  $archive_dir = 'swift-3.1.1-RELEASE-ubuntu16.04',
-  $source_url  = 'https://swift.org/builds/swift-3.1.1-release/ubuntu1604/swift-3.1.1-RELEASE/swift-3.1.1-RELEASE-ubuntu16.04.tar.gz',
+  $bin_dir      = '/opt/swagger-codegen/bin/',
+  $langs_dir    = '/opt/swagger-codegen/langs/',
+  $stage_dir    = '/tmp/packer-swaggy-c',
+  $archive_dir  = 'swift-3.1.1-RELEASE-ubuntu16.04',
+  $archive_file = 'swift-3.1.1-RELEASE-ubuntu16.04.tar.gz',
+  $source_url   = 'https://swift.org/builds/swift-3.1.1-release/ubuntu1604/swift-3.1.1-RELEASE/swift-3.1.1-RELEASE-ubuntu16.04.tar.gz',
 ) {
 
   package { 'clang':
@@ -16,12 +18,23 @@ class swagger_codegen::langs::swift (
     provider => apt,
   }
 
+  wget::fetch { 'swift-fetch':
+    source      => $source_url,
+    destination => "${stage_dir}/${archive_file}",
+    timeout     => 0,
+    verbose     => true,
+  } -> exec { 'swift-untar':
+    command => "tar xf ${stage_dir}/${archive_file}",
+    cwd     => $langs_dir,
+    path    => ['/bin','/usr/bin'],
+  }
+
   archive { $langs_dir:
     ensure       => present,
     extract      => true,
     extract_path => $langs_dir,
     source       => $source_url,
-    cleanup      => true,
+    cleanup      => false,
   }
 
   file { "${bin_dir}/swift-info.sh":
